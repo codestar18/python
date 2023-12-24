fe1024239f91a149b98778b3ac4546d42624230b
codestar18 <149290667+codestar18@users.noreply.github.com>
2023-12-24 09:36:43 +0530

    Update WhileLoop_Ex.py

--- WhileLoop_Ex.py
+++ WhileLoop_Ex.py
@@ -9,3 +9,8 @@ while i<3:
     else:
         print("You lost")
     i = i+1
+else:
+    print("your chances are over")
+
+#While loop also have else clause like if
+#it will get executed when while loop fails
