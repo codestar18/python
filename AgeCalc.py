ccd3b47606df4b4d6b79a02c7d046abbf3709903
codestar18 <149290667+codestar18@users.noreply.github.com>
2023-12-24 08:00:18 +0530

    Update_AgeCalc.py

--- AgeCalc.py
+++ AgeCalc.py
@@ -1,14 +1,10 @@
-birthYear = input("Enter the year you are born:")
-print(type(birthYear))
-print(type(bool(birthYear)))
-age = 2023 - int(birthYear)
-print(type(float(age)))
-print("You are "+ str(age) +"y of age")
+weight_in_pounds = int(input("Enter weight in pounds(lbs):"))
+converted_weight = weight_in_pounds*0.45
+print("Weight in Kilograms:"+str(converted_weight))
 
-""" when you take the input you are basically 
-taking the input in the form of a string 
-so when we perform mathematical calculation 
-int()
-str()
-float()
-bool()"""
+"""1 kilogram = 2.20462 pounds
+1 pound = 0.453592"""
+
+weight_in_kilograms = float(input("Enter the weight in kilograms:"))
+converted_weight = weight_in_kilograms / 0.45
+print("Weight in pounds:"+str(converted_weight))
