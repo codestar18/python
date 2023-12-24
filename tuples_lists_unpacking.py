32e712400e324bbd75af2332f970b91047d4526c
codestar18 <149290667+codestar18@users.noreply.github.com>
2023-12-24 11:06:44 +0530

    Add files via upload

---
+++ tuples_lists_unpacking.py
@@ -0,0 +1,19 @@
+coordinates = (1, 2, 3)
+x = coordinates[0]
+y = coordinates[1]
+z = coordinates[2]
+sum = x+y+z
+print(sum)
+
+
+#or
+#this is coool
+x,y,z = coordinates
+sum = x+y+z
+print(sum)
+
+#unpacking with lists
+coordinates = [1,2,3]
+x,y,z = coordinates
+sum=x+y+z
+print(sum)
