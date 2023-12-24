fe05053fb3e88bd8e7e725966adda8f5d4fab705
codestar18 <149290667+codestar18@users.noreply.github.com>
2023-12-24 08:42:37 +0530

    Add files via upload

---
+++ Arithemtice_operation_funcs.py
@@ -0,0 +1,8 @@
+import math
+x = 2.5
+print(round(x))#rounds off to floor value if its <=.5 or rounds to ceil value
+print(abs(-2.9))#returns only positive functions
+print(math.ceil(x))
+print(math.log(8,2))
+print(math.factorial(5))
+print(math.comb(4,2))#combination formula without repetition
