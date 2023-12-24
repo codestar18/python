c4815fa084136f123ea1227b271fc090e5f1f9c1
codestar18 <149290667+codestar18@users.noreply.github.com>
2023-12-24 08:01:23 +0530

    Update AgeCalc.py

--- AgeCalc.py
+++ AgeCalc.py
@@ -2,8 +2,8 @@ weight_in_pounds = int(input("Enter weight in pounds(lbs):"))
 converted_weight = weight_in_pounds*0.45
 print("Weight in Kilograms:"+str(converted_weight))
 
-"""1 kilogram = 2.20462 pounds
-1 pound = 0.453592"""
+"""1 kilogram = 0.45 pounds
+"""
 
 weight_in_kilograms = float(input("Enter the weight in kilograms:"))
 converted_weight = weight_in_kilograms / 0.45
