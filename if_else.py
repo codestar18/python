0afbb3870105d11efc55636519982bafe7ece7d0
codestar18 <149290667+codestar18@users.noreply.github.com>
2023-12-24 08:49:15 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,14 @@
+is_hot = False
+
+if is_hot:
+    print("It's a hot day ,drink more water")
+else:
+    print("It's a cold day, wear warm clothes")
+print("Enjoy your day")
+
+if 6%2== 0:
+    print("Great")
+elif 3%3==0:
+    print("Ok")
+else:
+    print("Bad")
