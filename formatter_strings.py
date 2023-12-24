08782af67f442b404733bb835e73ffac606b2850
codestar18 <149290667+codestar18@users.noreply.github.com>
2023-12-24 08:14:35 +0530

    Add files via upload

---
+++ formatter_strings.py
@@ -0,0 +1,10 @@
+first = "Jayasree"
+last = "Tummala"
+message = first + " [" + last + "] is a coder"
+print(message)
+
+#we use formatted strings to format strings easily
+""" use prefix f to start
+ a formatted string and use {} to create gap in the string"""
+msg = f'{first} [{last}] is a coder'
+print(msg)
