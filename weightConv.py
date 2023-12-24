4464351519cf47a7055e29899106e7dba74ad698
codestar18 <149290667+codestar18@users.noreply.github.com>
2023-12-24 07:57:30 +0530

    Add files via upload

---
+++ weightConv.py
@@ -0,0 +1,10 @@
+weight_in_pounds = int(input("Enter weight in pounds:"))
+converted_weight = weight_in_pounds/2.204624
+print("Weight in Kilograms:"+str(converted_weight))
+
+"""1 kilogram = 2.20462 pounds
+1 pound = 0.453592"""
+
+weight_in_kilograms = float(input("Enter the weight in kilograms:"))
+converted_weight = weight_in_kilograms * 2.204524
+print("Weight in pounds:"+str(converted_weight))
