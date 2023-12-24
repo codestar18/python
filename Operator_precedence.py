59a7b9fee257d7740eb60c9b1e5987fb3e8dadfe
codestar18 <149290667+codestar18@users.noreply.github.com>
2023-12-24 08:34:44 +0530

    Add files via upload

---
+++ Operator_precedence.py
@@ -0,0 +1,18 @@
+print(10/3)
+print(10//3)#returns integer
+print(10%3)
+print(2**3)#powers
+x = 2
+x += 3
+print(x)
+
+#Operator Precedence
+x = 10+3*2-2**3
+print(x)
+
+x = 10+3*2**3
+print(x)
+#Paranthesis>Exponentions>Mul/Div>Add/Sub
+
+x = (2+3) * 10 -3
+print(x)
