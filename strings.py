5f1847caf5a304a878fdb9758bf6abdff61e97cf
codestar18 <149290667+codestar18@users.noreply.github.com>
2023-12-24 08:10:53 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,12 @@
+course = "Python's course for Beginners"
+another_course = course[:]
+print(course)
+print(another_course)
+print(course[4:8])#on's
+print(course[-2]) #r
+print(course[0:])
+print(course[1:-1])
+
+#Exercise
+name = "Jennifer"
+print(name[1:-1])
